8e49a873cf8d6fc2a24a9ff0d0bcacbad8f689f8
Codrut Befu <codrut_befu@yahoo.com>
2025-08-16 16:11:59 +0300

    Update OrderController.php

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -16,50 +16,46 @@ class OrderController extends Controller
         $orderBookingInfo = $orderInfo['custom_info'];
         $bookedRooms = [];
         foreach ($orderInfo['items'] as $item) {
-            $x = 0;
-            while ($x < $item['quantity']) {
-                $rezervare = new Rezervarehotel();
-                $roomsIds = array_map(fn($id) => (int)trim($id), explode(',', $item['product_meta_input']['_hotel_room_number'][0]));
-                $hotelId = $item['product_meta_input']['_hotel_id'][0];
-                $tipCamera = $item['product_meta_input']['_hotel_room_type_long'][0];
-                // Calculate number of nights from start_date and end_date
-                $start = new \DateTime($orderBookingInfo['start_date']);
-                $end = new \DateTime($orderBookingInfo['end_date']);
-                $pret = $item['subtotal']/$item['quantity'];
-                $numberOfNights = $start->diff($end)->days;
-                // Populate $freeRoomsIds with values in $roomsIds but not in $bookedRooms
-                $freeRoomsIds = array_values(array_diff($roomsIds, $bookedRooms));
-                
-                $roomNumber = $rezervarehotel->getRoomNumber(
-                    $freeRoomsIds,
-                    $orderBookingInfo['start_date'],
-                    $orderBookingInfo['end_date'],
-                    $hotelId
-                );
 
-                $bookedRooms[] = $roomNumber[0];
-                $rezervare->idcl = '1'; //igclient
-                $rezervare->idclagentie1 = 0;
-                $rezervare->idclagentie2 = 0;
-                $rezervare->datas = $orderBookingInfo['start_date'];
-                $rezervare->dataf = $orderBookingInfo['end_date'];
-                $rezervare->camera = $roomNumber[0];
-                $rezervare->tipcamera = $tipCamera;
-                $rezervare->nrnopti = $numberOfNights;
-                $rezervare->nradulti = 2;
-                $rezervare->nrcopii = 0;
-                $rezervare->tipmasa = 'Fara MD';
-                $rezervare->prettipmasa = $pret;
-                $rezervare->pachet  = $tipCamera;
-                $rezervare->pretcamera = $pret;
-                $rezervare->pretnoapte = $pret;
-                $rezervare->total = $pret;
-                $rezervare->idfirma = 1;
-                $rezervare->utilizator = 'Web';
-                $rezervare->save();
-                $x++;
-               
-            }
+            $rezervare = new Rezervarehotel();
+            $roomsIds = array_map(fn($id) => (int)trim($id), explode(',', $item['product_meta_input']['_hotel_room_number'][0]));
+            $hotelId = $item['product_meta_input']['_hotel_id'][0];
+            $tipCamera = $item['product_meta_input']['_hotel_room_type_long'][0];
+            // Calculate number of nights from start_date and end_date
+            $start = new \DateTime($orderBookingInfo['start_date']);
+            $end = new \DateTime($orderBookingInfo['end_date']);
+            $pret = $item['subtotal'] / $item['quantity'];
+            $numberOfNights = $start->diff($end)->days;
+            // Populate $freeRoomsIds with values in $roomsIds but not in $bookedRooms
+            $freeRoomsIds = array_values(array_diff($roomsIds, $bookedRooms));
+
+            $roomNumber = $rezervarehotel->getRoomNumber(
+                $freeRoomsIds,
+                $orderBookingInfo['start_date'],
+                $orderBookingInfo['end_date'],
+                $hotelId
+            );
+
+            $bookedRooms[] = reset($roomNumber);
+            $rezervare->idcl = '1'; //igclient
+            $rezervare->idclagentie1 = 0;
+            $rezervare->idclagentie2 = 0;
+            $rezervare->datas = $orderBookingInfo['start_date'];
+            $rezervare->dataf = $orderBookingInfo['end_date'];
+            $rezervare->camera = reset($roomNumber);
+            $rezervare->tipcamera = $tipCamera;
+            $rezervare->nrnopti = $numberOfNights;
+            $rezervare->nradulti = 2;
+            $rezervare->nrcopii = 0;
+            $rezervare->tipmasa = 'Fara MD';
+            $rezervare->prettipmasa = $pret;
+            $rezervare->pachet  = $tipCamera;
+            $rezervare->pretcamera = $pret;
+            $rezervare->pretnoapte = $pret;
+            $rezervare->total = $pret;
+            $rezervare->idfirma = 1;
+            $rezervare->utilizator = 'Web';
+            $rezervare->save();
         }
         // Process each item in the order
         // For example, you might want to validate or save each item
