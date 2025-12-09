b34e4eac0fe726f964990a62aa9dd8134d6fcd02
Codrut Befu <codrut_befu@yahoo.com>
2025-12-09 22:36:24 +0200

    Update invoice.blade.php

--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -8,13 +8,13 @@
    <p>Bună ziua,</p>
    <p>Vă mulțumim pentru interesul acordat serviciilor noastre!</p>
    <p>Pentru orice întrebări sau nelămuriri, nu ezitați să ne contactați.</p>
-   <p>Parerea dumneavoastra este foarte importanta pentru noi si de aceea am aprecia foarte mult un feedback cu privire la experiența serviciilor noastre, prin intermediul adresei noastre de e-mail: noblesse@resortparadis.ro. </p>
-   <p>Pentru informatii suplimentare va stam la dispozitie atat pentru serviciile noastre!</p>
+   <p>Parerea dumneavoastra este foarte importanta pentru noi si de aceea am aprecia foarte 
+    mult un feedback cu privire la experiența serviciilor noastre, 
+    prin intermediul adresei noastre de e-mail: noblesse@resortparadis.ro. </p>
+   <p>Pentru informatii suplimentare va stam la dispozitie!</p
    <p>Va multumim,</p>
    <p>Echipa Noblesse</p>
-
    <p>Web : <a href="https://resortparadis.ro/">https://resortparadis.ro/</a></p>
-
    <p>Contact : +40 748 182 282</p>
 </body>
 </html>
