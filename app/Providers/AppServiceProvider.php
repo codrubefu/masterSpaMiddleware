b1ef76f9494cc46afd6f0db174d7e4c3564f942b
Codrut Befu <codrut_befu@yahoo.com>
2026-03-29 22:06:12 +0300

    Add web controller and page for model save logs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::saved(function (Model $model): void {
+            $route = request()?->route();
+            $controllerAction = $route?->getActionName();
+
+            Log::channel('model_saves')->info('Model saved', [
+                'saved_at' => now()->toDateTimeString(),
+                'model' => $model::class,
+                'table' => $model->getTable(),
+                'primary_key' => $model->getKey(),
+                'controller' => $controllerAction ?? 'N/A (non-http context)',
+            ]);
+        });
     }
 }
